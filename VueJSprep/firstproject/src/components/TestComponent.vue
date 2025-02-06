6aefd390137e1b4c2883614c6e7323016b4d54e4
prashantguptah <prashantegupta141@gmail.com>
2025-02-06 11:36:11 +0530

    added life cycle methods

--- VueJSprep/firstproject/src/components/TestComponent.vue
+++ VueJSprep/firstproject/src/components/TestComponent.vue
@@ -3,6 +3,7 @@
   <div>
     <h1>Hello {{ input }}</h1>
     <input type="text" v-model="input">
+    {{ date }}
 
     <br>
     <button v-on:click="clear">Clear</button>
@@ -18,9 +19,28 @@ export default {
   name: "TestComponent",
   data() {
     return {
-      input:""
+      input:"Prashant",
+      date : false
     };
   },
+
+  created: function(){
+    console.log("Created "+ this.input)
+    this.date = new Date()
+  },
+  mounted: function(){
+    console.log("mounted"+ this.date)
+    this.date = new Date()
+  },
+  updated: function(){
+    console.log("updated" +this.date)
+    // this.date = new Date()   error =  Maximum recursive updates
+  },
+  unmounted: function(){
+    console.log("destroyed" + this.date)
+    this.date = new Date()
+  },
+
   methods:{
     clear: function(){
         this.input = ""
