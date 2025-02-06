1883200b0d10a3381452b61b62c2b84de3e1e786
prashantguptah <prashantegupta141@gmail.com>
2025-02-06 11:12:54 +0530

    created TextComponent and simple form

---
+++ VueJSprep/firstproject/src/components/TestComponent.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div>
+    <h1>Hello {{ input }}</h1>
+    <input type="text" v-model="input">
+
+    <br>
+    <button v-on:click="clear">Clear</button>
+    <button v-on:click="capitals">Capital</button>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      input:""
+    };
+  },
+  methods:{
+    clear: function(){
+        this.input = ""
+    },
+    capitals: function(){
+     this.input = this.input.toUpperCase()
+    }
+  }
+};
+</script>
+
+
+
+<style>
+
+</style>
