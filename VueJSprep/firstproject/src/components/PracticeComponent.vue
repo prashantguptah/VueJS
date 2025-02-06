a1274aac2be9426801fb102530913d19f9781ab4
prashantguptah <prashantegupta141@gmail.com>
2025-02-06 13:35:10 +0530

    implemented test color change with select options

--- VueJSprep/firstproject/src/components/PracticeComponent.vue
+++ VueJSprep/firstproject/src/components/PracticeComponent.vue
@@ -2,7 +2,68 @@
 
 
 <div :style="styleObject">Hello Vue</div>
+
+
+
+<!-- form input bindings -->
+ 
+
+<!-- input text -->
+<p>message is: {{ message }}</p>
+<input type="text" placeholder="Enter the data" v-model="message">
+
+
+<!-- multiline  text-->
+
+
+ <p>Multiline Text {{ multimessage }}</p>
+ <textarea placeholder="Enter inputs" v-model="multimessage"></textarea>
+
+
+
+ <!-- checkbox -->
+
+
+  <input type="checkbox" id="checkbox" v-model="checked">
+  <label for="checkbox">{{ checked }}</label>
+
+
+
+  <!-- multiple checkbox -->
+
+
+  <div>Checked names: {{ checkedNames }}</div>
+
+  <input type="checkbox" id="Jack" value="Jack" v-model="checkedNames">
+  <label for="Jack">Jack</label>
+  <input type="checkbox" id="Sam" value="Sam" v-model="checkedNames">
+  <label for="Sam">Sam</label>
+  <input type="checkbox" id="Sunil" value="Sunil" v-model="checkedNames">
+  <label for="Sunil">Sunil</label>
+  <input type="checkbox" id="Ravi" value="Ravi" v-model="checkedNames">
+  <label for="Ravi">Ravi</label>
+
+
+
+
+  <!-- select option -->
+
+
+  <div>Selected: {{ selected }}</div>
+
+  <select  v-model="selected">
+    <option disabled value="">Please select one</option>
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select>
+   
 </template>
+
+
+
+
+
 
 
 <script>
@@ -11,12 +72,18 @@ export default{
     name: "PracticeComponent",
     data(){
         return {
+            message:"",
+            multimessage:"",
+            checked:true,
+            checkedNames:[],
+            selected:"",
             styleObject:{
                 color: "blue",
                 fontsize: 20
             }
         }
-    }
+    },
+    
 }
 
 
--- VueJSprep/firstproject/src/components/TestComponent.vue
+++ VueJSprep/firstproject/src/components/TestComponent.vue
@@ -7,18 +7,59 @@
     <div :class="{red:isRed}">
       <h1>Hello {{ input }}</h1>
     </div>
+
+
+    
    
     <input type="text" v-model="input">
-    {{ date }}
+    {{ date }}   
 
     <br>
     <button v-on:click="clear">Clear</button>
     <button v-on:click="capitals">Capital</button>
 
-    <hr>
+    <input :checked="isRed" v-model="isRed" type="checkbox" @click="red"/> Red
+   
     
 
-    <input :checked="isRed" v-model="isRed" type="checkbox" @click="red"/> Red
+    
+
+
+
+
+    <div :style="{color:colored}">
+      <h1>Hello {{ input }}</h1>
+    </div>
+
+
+    <!--                 OR                -->
+
+
+      <!-- <div v-bind:style="{color: color_value.color}">
+        <h1>Hello {{ input }}</h1>
+    </div> -->
+
+    <select  v-model="colored">
+      <option disabled value="">Select the color</option>
+      <option value="red">red</option>
+      <option value="blue">blue</option>
+      <option value="green">green</option>
+      <option value="black">black</option>
+      <option v-for="(col,index) in colors" :key="index">{{ col }}</option>
+    </select>
+
+
+    <!--             OR            -->
+
+
+    <!-- <select v-model="color_value">
+            <option v-bind:value="{color: 'red'}">red</option>
+            <option v-bind:value="{color: 'yellow'}">yellow</option>
+            <option v-bind:value="{color: 'black'}">black</option>
+            <option v-bind:value="{color: 'orange'}">orange</option>
+            <option v-for="(col, index) in colors" v-bind:key="index" v-bind:value="{color: col}">{{col}}</option>
+        </select> -->
+    <hr>
 
 
    
@@ -35,7 +76,12 @@ export default {
     return {
       input:"Prashant",
       date : false,
-      isRed: false
+      isRed: false,
+      colored:"",
+      colors: ["red","yellow", "black", "orange"]
+      // color_value: {
+      //     color: "black"
+      // },
     };
   },
 
